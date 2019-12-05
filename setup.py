2c6ff66458b5031853424ae33ea38eb0d500a3bb
Pay Giesselmann <pay.giesselmann@web.de>
2019-12-05 13:01:35 +0100

    v0.4.1

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='STRique',
-    version='0.3.0',
+    version='0.4.1',
     author='Pay Giesselmann',
     author_email='giesselmann@molgen.mpg.de',
     description='Nanopore raw signal repeat detection',
